96df0102d1a41efeb31ff26123c859bafa36fa5c
jfProj2 <jfProj2@gmail.com>
2017-03-14 15:00:02 -0400

    Testing editor out and officially starting on prj2
    So just made the test.cpp file into our main.cpp by changing the name. 
    
    Started some basic documentation for the header of the main.cpp file
    
    Added a few includes I think we will need as well as instantiating the global namespace std.
    
    Return value set to 22 (my habit)

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/**
+PROJECT 2
+#########
+Fick-Joiner Productions
+#########
+Due: March 27th, 2017 @ 11:55 PM
+Professor: Michael Cotterell
+
+File: main.cpp
+#########
+<FINISH DESCRIPTION>
+This is the main file...
+<FINISH DESCRIPTION>
+**/
+
+#include <iostream>
+#include <iomanip>
+#include <ncurses.h>
+#include <menu.h>
+#include <cstdlib>
+#include "io.h"
+#include "tui.h"
+
+using namespace std;
+
+int main() //At somepoint we need to include execution-time parameters to the main method
+{
+  return 22; //Returning this # is my habit :P
+}
