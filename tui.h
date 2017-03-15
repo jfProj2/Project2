88cd82a6d31ddd71f3be0c6aa68b212080a3240f
jfProj2 <jfProj2@gmail.com>
2017-03-15 14:04:04 -0400

    Add files via upload

---
+++ tui.h
@@ -0,0 +1,25 @@
+#ifndef TUI_H
+#define TUI_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class tui {
+
+  //inst vars
+  io();
+
+ public:
+
+  /**Opens tui with no file specified**/
+  tui();
+
+  /**Opens tui with file specified**/
+  tui(const char * fileptr); 
+
+  /**Destructor*/
+  ~tui();
+  createMainWin();
+  createMenu();
+  createErrWin();
