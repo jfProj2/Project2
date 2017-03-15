9451a13b3724f7a66e691a083a3e781764fa9e5b
jfProj2 <jfProj2@gmail.com>
2017-03-15 14:34:27 -0400

    Add files via upload

--- tui.h
+++ tui.h
@@ -2,27 +2,37 @@
 #define TUI_H
 #include <iostream>
 #include <string>
+#include <ncurses.h>
+#include "io.h"
 
 using namespace std;
 
 class tui {
 
   //inst vars
-  io();
+  WINDOW *main_win;
+  //WINDOW *menu_win; //If these windows aren't always here, should they be
+  //WINDOW *err_win;  // instance vars? 
+  FILE* file
+  
 
  public:
 
-  /**Opens tui with no file specified**/
+  /**Opens initial tui with no file specified**/
   tui();
 
-  /**Opens tui with file specified**/
-  tui(const char * fileptr); 
+  /**Opens initial tui with file specified**/
+  tui(const char * fileptr);
+
+  /**Opens tui with file specified in menu**/
+  tui(FILE* f);
 
   /**Destructor*/
   ~tui();
-  createMainWin();
-  createMenu();
-  createErrWin();
+  WINDOW *create_main_win();
+  WINDOW *create_menu_win();
+  WINDOW *create_err_win();
+  
 
 };
 
