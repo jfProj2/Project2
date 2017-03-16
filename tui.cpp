1c7c76d28adf63bec8f88cb406a9c7280af0d2b8
jfProj2 <jfProj2@gmail.com>
2017-03-16 16:25:11 -0400

    the tui functions... needs ALOT of work

---
+++ tui.cpp
@@ -0,0 +1,42 @@
+/**
+PROJECT 2
+**************
+
+File: tui.cpp
+
+Date: 3/16/2017
+
+Authors: Ryan Joiner (810638856)
+         Emma Fick   (810609119)
+
+Prof. Michael Cotterell
+**************
+
+<NEED DESCRIPTION>
+
+This is the implementation file for the tui class's methods...
+
+<NEED DESCRIPTION>
+**/
+
+//INCLUDES
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <cstdio>
+#include <fstream>
+#include "io.h"
+#include "tui.h"
+
+using namespace std;
+
+TUI::~TUI(){
+  
+
+}
+
+TUI::TUI(){
+
+}
