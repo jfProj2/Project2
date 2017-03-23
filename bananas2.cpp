255601010f86c59af0bf937b706f4b3a0b6e6eb5
jfProj2 <jfProj2@gmail.com>
2017-03-23 16:50:50 -0400

    Add files via upload

--- bananas2.cpp
+++ bananas2.cpp
@@ -29,21 +29,36 @@ using namespace std;
 
 int main(const int argc, const char * argv[]);
 
-//void run_editor(string fileName);
+void run_editor(string fileName);
 
-bool init_menu_window(MENU* menu, WINDOW* win, ITEM* current);
+int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x);
 
+string load_file(int &fd, string fileName);
 
+bool close_file(int &fd);
 
 int main(const int argc, const char * argv[]){
-  
+  //int errorNum = 0;
   if(argc == 1){
 
-    run_editor(nullptr);
+    run_editor("no file");
 
   }
   else if(argc == 2){
-    run_editor(argv[1]);
+    //int FileDesc = IO::openFile(argv[1]);
+    //If failure to open, create an error window inside the editor
+    /**
+    if(FileDesc < 0){
+      errorNum = errno;
+      printf("1730ed: [%s]: Error.\n", argv[1].c_str());
+      printf("errno: %i - %s\n\n", errorNum, strerror(errorNum));
+      return EXIT_FAILURE;
+    }
+    else{
+
+
+    }
+    **/
   }
   else{
     printf("ERROR: Too many arguments to ./1730ed\n");
