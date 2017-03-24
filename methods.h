09c45a6533eb51945c668737242e5973eca4c73c
jfProj2 <jfProj2@gmail.com>
2017-03-24 17:39:52 -0400

    Add files via upload
    Things it does now:
    - Can type mostly, both with existing and new files
    - Can save files via menu
    - Exits via menu only now
    -All menu options active
    - Alert windows started (ex "Save file?")
    - I think there are colors?
    
    Needed:
    - Typing worked out (backspace doesn't work, arrow keys do weird things)
    - Alert windows functional
    - Lots of error checking
    - Menu option logic - Big one

--- methods.h
+++ methods.h
@@ -26,53 +26,48 @@ using namespace std;
 
 
 //PROTOTYPES
+//IN ORDER, SIMPLEST FIRST
 string load_file(string filename);
 bool close_file(int &fd);
-void run_editor(const char * fileptr);
 string get_filename(const char * fileptr);
+void run_editor(const char * fileptr);
+string add_char(char ch, string data); //NEW
+string remove_char(string s); //NEW
+bool file_exists(const char * fileptr); //NEW
 void file_to_screen(string data, WINDOW * win, int h, int w);
+void save_file(string filename, string data); //NEW
 int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x);
-string add_char(char ch, string data);
-string remove_char(string s);
-void save_file(string filename, string data);
-bool file_exists(const char * fileptr);
+void run_editor(const char * fileptr);
+int show_alert_window(int code, int h, int w, int y, int x); //NEW
 
-string get_filename(const char * fileptr){
-  if (fileptr == nullptr){
-    return "Untitled";
-  }
-  else {
-    string fn = fileptr;
-    return fn;
-  }
-}
-
+/**returns string of chars in file
+   TO DO: error handling */
 string load_file(string filename){
-  int fd = open(filename.c_str(), O_RDWR | /*O_CREAT |*/ O_APPEND);
+  //took away O_CREAT so we don't auto create files
+  //(made more sense for project)
+  int fd = open(filename.c_str(), O_RDWR | O_APPEND);
   if(fd < 0){
+    //ERROR CHECK HERE
     //int errorNum = errno;
     //error window
     //return int referring to type of error
     return "";
-  }
-  else{
+  }else{
     const int BUFF_SIZE = 1024;
     char buffer[BUFF_SIZE];
     int n = 0;
     string data = "";
     while((n = read(fd, buffer, BUFF_SIZE)) > 0){
     //write?
-    	for(int i = 0; i < n; i++){
-
-	  if(buffer[i] != '\n'){
-	   	data += buffer[i];
-	  }
+      for(int i = 0; i < n; i++){
+	data += buffer[i];      
       	}
     }
     return data;
   }
 }
 
+/**TO DO: all destructors and stuff*/
 bool close_file(int &fd){
   fd = close(fd);
   if(fd < 0){
@@ -84,84 +79,20 @@ bool close_file(int &fd){
   return true;
 }
 
-void run_editor(const char *fileptr){
-  WINDOW *term_win;
-  WINDOW *edit_win;
-  string filename = get_filename(fileptr);
-  bool exists = file_exists(fileptr);
-  initscr();
-  cbreak();
-  noecho();
-  int term_h, term_w;
-  getmaxyx(stdscr, term_h, term_w);
-  int menu_h = term_h/2;
-  int menu_w = term_w/2;
-  int menu_x = (term_w / 4), menu_y = (term_h / 4);
-  int edit_x = 1, edit_y = 1;
-  int edit_h = (term_h * 0.90);
-  int edit_w = (term_w * 0.90);
-  term_win = newwin(term_h, term_w, 0, 0);
-  edit_win = derwin(term_win, edit_h, edit_w, edit_x, edit_y);
 
-  //FIGURE OUT PADS?
-  //edit_win = newpad(edit_h,edit_w); 
-  
-  touchwin(edit_win);
-  keypad(term_win, true);
-  keypad(edit_win, true);
-  refresh();
-  mvwprintw(term_win, 0, 0, "F1: Menu");
-  mvwprintw(term_win, 0, (term_w/2)-8, "CSCI 1730 Editor");
-  mvwprintw(term_win, (term_h-1), 0, filename.c_str());
-
-  string text = load_file(filename);
-  file_to_screen(text, edit_win, edit_h, edit_w);
-  
-  wrefresh(term_win);
-  wrefresh(edit_win);
-  //prefresh(edit_win, edit_y,edit_x, edit_y,edit_x, edit_h, edit_w);
-  box(edit_win, '*', '*');
-  int key = 1;
-  bool running = true;
-  string appended = "";
-  while(running){
-    bool saved = false;
-    key = wgetch(edit_win);
-    text = add_char(key, text);
-    file_to_screen(text, edit_win, edit_h, edit_w);
-
-    switch(key){
-    case KEY_F(1):
-      int selection = show_menu_window(menu_h, menu_w, menu_y, menu_x);
-      wrefresh(term_win);
-      if(selection == 0){
-	 wrefresh(term_win);
-         wrefresh(edit_win);	 
-      }
-      else if(selection == 1){
-	//open
-      }
-      else if(selection == 2){
-	save_file(filename, text);
-      }
-      else if(selection == 3){
-	//save as
-      }
-      else if(selection == 4){
-	running = false;
-      }
-      break;
-    }
-    wrefresh(term_win);
-    wrefresh(edit_win);
+string get_filename(const char * fileptr){
+  if (fileptr == nullptr){
+    return "Untitled";
   }
-  delwin(edit_win);
-  delwin(term_win);;
-  endwin();
+  else {
+    string fn = fileptr;
+    return fn;
+  }
 }
 
-
-
+/**use for typing
+   TO DO: figure out how to deal with weird chars
+   eg arrow keys*/
 string add_char(char ch, string s){
   string data = "";
   int len = s.size();
@@ -175,6 +106,8 @@ string add_char(char ch, string s){
     return data;
 }
 
+/**use for typing backspace, doesn't really work
+   TO DO: figure out how to backspace*/
 string remove_char(string s){
   string data = "";
   for(int i = 0; i < (s.size() - 1); i++){
@@ -184,8 +117,42 @@ string remove_char(string s){
   return s;
 }
 
-int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x){
-  
+bool file_exists(const char * fileptr){
+  return(fileptr != nullptr);
+}
+
+/**TO DO: weird xy-issues*/
+void file_to_screen(string data, WINDOW * win, int height, int width){
+  int x = 1;
+  int y = 1;
+  int w = width - 1;
+  for(int i = 0; i < data.size(); i++){
+    char ch = data[i];
+      mvwprintw(win, x, y, "%c", ch);
+      if(y == (w - 1) || ch == '\n'){
+	x++;
+	y = 1;
+	mvwprintw(win, x, y,"%c", '\n');
+      }
+      y++;
+  }
+}
+
+/**saves file by recreating file and writing over, basically
+   TO DO: error check */
+void save_file(string filename, string s){
+  int fd = open(filename.c_str(),O_RDWR, O_TRUNC); //error check here
+  string data = s;
+  int n = s.size();
+  char * buffer = new char[n];
+  buffer = (char*)data.c_str();
+  if(write(fd, buffer, n) == -1){
+    //error check here
+  }
+}
+
+/**Menu is now interactive with enter key*/
+int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x){  
   WINDOW *menu_win;
   WINDOW *menu_subwin;
   MENU *main_menu;
@@ -225,9 +192,7 @@ int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x){
       menu_driver(main_menu, REQ_UP_ITEM);
       break;      
     case '\n':
-      //MORE TO DO HERE
-      //menu_driver(main_menu,REQ_TOGGLE_ITEM);
-      // cur_item= current_item(main_menu);
+      //MORE TO DO HERE?
       selected = true;
       break;
     }
@@ -265,45 +230,151 @@ int show_menu_window(int menu_h, int menu_w, int menu_y, int menu_x){
     return choice; //USER EXITS MENU (F1 Pressed)
 }
 
+/**Program now only exits thru menu, alert windows started
+   Save mostly works!!
+   TO DO: menu logic, errors!
+*/
+void run_editor(const char *fileptr){
+  WINDOW *term_win;
+  WINDOW *edit_win;
+  string filename = get_filename(fileptr);
+  bool exists = file_exists(fileptr);
+  initscr();
+  start_color();
+  init_pair(1, COLOR_BLACK, COLOR_CYAN);
+  init_pair(2, COLOR_WHITE, COLOR_MAGENTA);
+  cbreak();
+  noecho();
+  int term_h, term_w;
+  getmaxyx(stdscr, term_h, term_w);
+  int menu_h = term_h/2;
+  int menu_w = term_w/2;
+  int menu_x = (term_w / 4), menu_y = (term_h / 4);
+  int edit_x = 1, edit_y = 1;
+  int edit_h = (term_h * 0.90);
+  int edit_w = (term_w * 0.90);
+  term_win = newwin(term_h, term_w, 0, 0);
+  edit_win = derwin(term_win, edit_h, edit_w, edit_x, edit_y);
 
-void file_to_screen(string data, WINDOW * win, int height, int width){
-  int x = 1;
-  int y = 1;
-  int w = width - 1;
-  for(int i = 0; i < data.size(); i++){
-    char ch = data[i];
-      mvwprintw(win, x, y, "%c", ch);
-      if(y == (w - 1) || ch == '\n'){
-	x++;
-	y = 1;
-	//mvwprintw(win, x, y,"%c", '\n');
+  //FIGURE OUT PADS? Not sure if necessary.
+ 
+  touchwin(edit_win);
+  keypad(term_win, true);
+  keypad(edit_win, true);
+  refresh();
+  attron(COLOR_PAIR(1));  //Attempting to turn on color
+  mvwprintw(term_win, 0, 0, "F1: Menu");
+  mvwprintw(term_win, 0, (term_w/2)-8, "CSCI 1730 Editor");
+  mvwprintw(term_win, (term_h-1), 0, filename.c_str());
+  attroff(COLOR_PAIR(1));
+  string text = load_file(filename);
+  file_to_screen(text, edit_win, edit_h, edit_w);
+  
+  wrefresh(term_win);
+  wrefresh(edit_win);
+  box(edit_win, '*', '*');
+  int key = 1;
+  bool running = true;
+  while(running){
+    bool saved = false;
+    key = wgetch(edit_win);
+    text = add_char(key, text);
+    file_to_screen(text, edit_win, edit_h, edit_w);
+
+    switch(key){
+    case KEY_F(1):
+      int selection = show_menu_window(menu_h, menu_w, menu_y, menu_x);
+      int alert_select;
+      wrefresh(term_win);
+      if(selection == 0){
+	 wrefresh(term_win);
+         wrefresh(edit_win);	 
       }
-      y++;
+      else if(selection == 1){
+	//open
+	alert_select = show_alert_window(1,menu_h, menu_w, menu_y, menu_x);
+	if(alert_select == 0){
+	   wrefresh(term_win);
+	   wrefresh(edit_win);
+	}
+      }
+      else if(selection == 2){
+	//save
+	save_file(filename, text);
+      }
+      else if(selection == 3){
+	//save as
+	alert_select = show_alert_window(3,menu_h, menu_w, menu_y, menu_x);
+	if(alert_select == 0){
+	   wrefresh(term_win);
+	   wrefresh(edit_win);
+	}
+      }
+      else if(selection == 4){
+	//exit
+	running = false;
+      }
+      break;
+    }
+    wrefresh(term_win);
+    wrefresh(edit_win);
   }
-  //mvwprintw(win, x, y, "%c", '\0');
+  delwin(edit_win);
+  delwin(term_win);;
+  endwin();
 }
 
-bool file_exists(const char * fileptr){
-  return(fileptr != nullptr);
+/**New: different alerts depending on the situation
+   TO DO: they won't disappear, menu options
+   PLAN: return an int (0 to exit, like with menu func)
+   For ex, user selected open -> "no" to save changes -> returns 3, maybe
+   or user selected save as -> "yes" to overwrite -> returns 8, maybe
+   NEED ALL OPTIONS OF WHAT USER COULD DO HERE
+ */
+int show_alert_window(int code, int h, int w, int y, int x){
+  WINDOW * alert_win = newwin(h,w,y,x);
+  //WINDOW * alert_subwin;
+  string mssg = "";
+  // MENU * alert_menu;
+  int key = 1;
+  int choice = 0;
+  if(code == 1){
+    mssg += "Save Changes?";
+  }else if(code == 2){
+    mssg += "Enter file name: ";
+  }else if(code == 3){
+    mssg += "Overwrite file?";
+  }
+  box(alert_win, '*', '*');
+  initscr();
+  mvwprintw(alert_win, 1, (w/2)-(mssg.size()/2), mssg.c_str());
+  cbreak();
+  wrefresh(alert_win);
+  int ch;
+  if((ch = getch()) != KEY_F(1)){
+    wrefresh(alert_win);
+  }
+  wrefresh(alert_win);
+  delwin(alert_win);
+  return choice;
+
 }
 
-void save_file(string filename, string s){
-  int fd = open(filename.c_str(),O_RDWR, O_TRUNC);
-  string data = s;
-  int n = s.size();
-  char * buffer = new char[n];
-  buffer = (char*)data.c_str();
-  if(write(fd, buffer, n) == -1){
-    //error check here
-  }
-}
+
+
+
+
+
+
+
+
 
 //can open existing file
 //save file
 //exit
 //menu can be used
 //typing - not backspace or random keys
-//
+//newlines weird when reopening
   
   
   
